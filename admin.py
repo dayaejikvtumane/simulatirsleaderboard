54ec054eb962be1b0f5a2722af5f5882e8df7749
gelik <136448734+mysitessa@users.noreply.github.com>
2025-05-10 22:59:46 +0300

    send CSV

--- admin.py
+++ admin.py
@@ -5,11 +5,10 @@ import csv
 import io
 
 
-
 async def admin_menu(update, context):
     keyboard = [
         [InlineKeyboardButton("Добавить админа", callback_data='add_admin')],
-        [InlineKeyboardButton("Проверить данные", callback_data='check_data')],
+        [InlineKeyboardButton("Изменить данные", callback_data='change_data')],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -18,14 +17,16 @@ async def admin_menu(update, context):
         reply_markup=reply_markup
     )
 
+
 async def button_click(update, context):
     query = update.callback_query
     await query.answer()
 
     if query.data == 'add_admin':
         await add_admin_handler(update, context)
-    elif query.data == 'check_data':
-        await check_data_handler(update, context)
+    elif query.data == 'change_data':
+        await change_data_handler(update, context)
+
 
 async def add_admin_handler(update, context):
     await context.bot.send_message(
@@ -35,30 +36,35 @@ async def add_admin_handler(update, context):
     context.user_data['awaiting_admin_id'] = True
 
 
-async def check_data_handler(update, context):
+async def change_data_handler(update, context):
     session = create_session()
-    sp_flight_results = []
-
-    for i in session.query(FlightResult).all():
-        sp_flight_results.append(str(i))
+    flights = session.query(FlightResult).all()
 
     csv_buffer = io.StringIO()
     writer = csv.writer(csv_buffer, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(["ID", "student_id", "simulator", "map_name", "flight_mode", "time", "photo_path", "date_added"])
 
-    for i in sp_flight_results:
-        data = i.split()
-        print(data)
-        writer.writerow([data[0], data[1], data[2], data[3], data[4], data[5]])
+    for flight in flights:
+        writer.writerow([
+            flight.id,
+            flight.student_id,
+            flight.simulator,
+            flight.map_name,
+            flight.flight_mode,
+            flight.time,
+            flight.photo_path,
+            flight.date_added
+        ])
 
     csv_buffer.seek(0)
     csv_bytes = io.BytesIO(csv_buffer.getvalue().encode('utf-8'))
-    csv_bytes.seek(0)
 
     await context.bot.send_document(
         chat_id=update.effective_chat.id,
         document=InputFile(csv_bytes, filename='Flight_Results.csv'),
-        caption="Flight_Results"
+        caption="Данные полетов"
     )
+
 
 async def handle_admin_message(update, context):
     if not context.user_data.get('awaiting_admin_id'):
